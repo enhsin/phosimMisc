091c42d36d1b7b5de373c4024ed569b0c33897c9
En-Hsin Peng <epeng@purdue.edu>
2015-06-02 23:10:06 -0400

    field angle to (ra,dec)

--- xyPosition.py
+++ xyPosition.py
@@ -6,11 +6,27 @@
 
 Usage: python xyPosition.py ra 0 1.7   [convert sky (deg) to image (micron)]
        python xyPosition.py xy 0.0 306089.82   [convert image (micron) to sky (deg)]
+       python xyPosition.py field 0 1.7   [convert field angle (deg) to image (micron)]
+
+
+      x = cos(dec)sin(ra)
+      y = sin(dec)
+      z = cos(dec)cos(ra)
+      p0 = (0, 0, 1)
+      n = (0, 0, 1)
+      l0 = (0, 0, 0)
+      l = (x, y, z)
+      d = (p0 -l0).n / (l.n)
+        = 1/z
+      p = d.l + l0
+        = (x/z, y/z, 1)
+      fx = atan2(1, x/z)
+      fy = atan2(1, y/z)
 
 """
 
 import sys
-from math import sin, cos, pi
+from math import sin, cos, pi, tan, atan2, atan
 from scipy.optimize import fmin_tnc
 
 DEGREE = pi/180.0
@@ -32,27 +48,75 @@ def xyPositionRA(alpha, delta, pointingRA = 0.0, pointingDec = 0.0, rotationAngl
     '''position in microns'''
     print x, y
 
+def xyPositionField(angleX, angleY):
+    x = focalLength*tan(angleX)
+    y = focalLength*tan(angleY)
+    print x, y
+
+def field2Sky2(angleX, angleY):
+    x = focalLength*tan(angleX)
+    y = focalLength*tan(angleY)
+    skyAngle(x, y)
+
+def field2Sky(fx, fy):
+    #x = cos(dec)sin(ra)
+    #y = sin(dec)
+    #z = cos(dec)cos(ra)
+    if fx == 0:
+        ra = fx
+        dec = fy
+    else:
+        x = tan(fx)  #x/z
+        y = tan(fy)  #y/z
+        ra = fx
+        dec = atan(y/(x/sin(ra)))
+    print '%.10f %.10f' % (ra/DEGREE, dec/DEGREE)
+
+def sky2Field(ra, dec):
+    x = cos(dec)*sin(ra)
+    y = sin(dec)
+    z = cos(dec)*cos(ra)
+    fx = atan2(x, z)
+    fy = atan2(y, z)
+    print '%.10f %.10f' % (fx/DEGREE, fy/DEGREE)
+
 def solveXY(sky, ra0, dec0, x0, y0):
     xp, yp = xyPosition(sky[0], sky[1], ra0, dec0)
     return (xp-x0)**2+(yp-y0)**2
 
 #Warning: use with caution
-def fieldAngle(x, y, rotationAngle = 0.0, pointingRA = 0.0, pointingDec = 0.0):
+def skyAngle(x, y, rotationAngle = 0.0, pointingRA = 0.0, pointingDec = 0.0):
     x0 = (x*cos(rotationAngle) - y*sin(rotationAngle))
     y0 = (x*sin(rotationAngle) + y*cos(rotationAngle))
     ra0 = pointingRA
     dec0 = pointingDec
     b=[(-pi,pi),(-pi/2,pi/2)]
-    mpar=fmin_tnc(solveXY, [0,pi/4], approx_grad=1, bounds=b, args=(ra0,dec0,x0,y0),messages=0)
+    mpar=fmin_tnc(solveXY, [0,pi/4], approx_grad=1, bounds=b, args=(ra0,dec0,x0,y0),messages=0, accuracy=1e-12)
     '''position in degrees'''
-    print mpar[0][0]/DEGREE, mpar[0][1]/DEGREE
+    print '%.10f %.10f' % (mpar[0][0]/DEGREE, mpar[0][1]/DEGREE)
 
 if sys.argv[1] == 'ra':
     alpha=float(sys.argv[2])*DEGREE
     delta=float(sys.argv[3])*DEGREE
     xyPositionRA(alpha,delta)
-else:
+elif sys.argv[1] == 'field':
+    alpha=float(sys.argv[2])*DEGREE
+    delta=float(sys.argv[3])*DEGREE
+    xyPositionField(alpha,delta)
+elif sys.argv[1] == 'fieldRA':
+    alpha=float(sys.argv[2])*DEGREE
+    delta=float(sys.argv[3])*DEGREE
+    field2Sky(alpha,delta)
+elif sys.argv[1] == 'fieldRA2':
+    alpha=float(sys.argv[2])*DEGREE
+    delta=float(sys.argv[3])*DEGREE
+    field2Sky2(alpha,delta)
+elif sys.argv[1] == 'raField':
+    alpha=float(sys.argv[2])*DEGREE
+    delta=float(sys.argv[3])*DEGREE
+    sky2Field(alpha,delta)
+elif sys.argv[1] == 'xy':
     x=float(sys.argv[2])
     y=float(sys.argv[3])
-    fieldAngle(x, y)
+    skyAngle(x, y)
 
