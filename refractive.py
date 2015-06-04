4f1439dca6a0356a67c500172c8e632e8ff77419
En-Hsin Peng <epeng@purdue.edu>
2015-06-04 16:18:10 -0400

    air refraction

--- refractive.py
+++ refractive.py
@@ -8,7 +8,7 @@ http://www.researchgate.net/profile/Laurent_Pilon/publication/5819871_Optical_co
 Optical constants of silica glass from extreme ultraviolet
 to far infrared at near room temperature
 """
-def refractive(l):
+def siRefractive(l):
     l2 = l*l
     t1 = 0.6961663*l2/(l2-0.0684043**2)
     t2 = 0.4079426*l2/(l2-0.1162414**2)
@@ -16,14 +16,22 @@ def refractive(l):
     n = (1+t1+t2+t3)**0.5
     return n
 
+def airRefractive(l,temperature=20,pressure=520,water_pressure=8):
+    l2 = l*l
+    n = 64.328 + 29498.1/(146 - 1/l2) + 255.4/(41 - 1/l2);
+    n = n*pressure*(1 + (1.049 - 0.0157*temperature)*1e-6*pressure)/720.883/(1 + 0.003661*temperature);
+    n = n - ((0.0624 - 0.000680/l2)/(1 + 0.003661*temperature)*water_pressure);
+    return 1.0+n/1e6
+
 def makeTable():
     out=open('silica_dispersion.txt','w')
     out.write("# media file for fused silica (corning)\n")
     out.write("# column 1 is wavelength (microns) and column 2 is index of refraction\n")
     for l in np.linspace(0.3, 1.2, 901):
-        out.write('    %.5f    %.9f\n' % (l,refractive(l)))
+        out.write('    %.5f    %.9f\n' % (l,siRefractive(l)))
     out.close()
 
-#n=refractive(float(sys.argv[1]))
-#print '%.10f' % n
-makeTable()
+#n=siRefractive(float(sys.argv[1]))
+n=airRefractive(float(sys.argv[1]))
+print '%.10f' % n
+#makeTable()
