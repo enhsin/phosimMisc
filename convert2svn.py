9ee13b91118fc6949c070fca1e09b8822deafa38
En-Hsin Peng <epeng@purdue.edu>
2015-10-14 17:42:00 -0400

    script to convert phosim git repository to diagrid svn repository

--- convert2svn.py
+++ convert2svn.py
@@ -1,5 +1,6 @@
 import os, sys, glob
 import filecmp, shutil
+import difflib
 
 def listFolder(fld):
     for f in glob.glob(fld+'/.*'):
@@ -9,21 +10,21 @@ def listFolderW(fld):
     for root, dirs, files in os.walk(fld):
         print root.replace(fld,'testcode'), len(files)
 
-def compareFiles(orgF,newF):
+def compareFiles(oldF,newF):
     if os.path.exists(newF):
-        if not filecmp.cmp(orgF,newF):
+        if not filecmp.cmp(oldF,newF):
             print 'update', newF
     else:
-        print 'new file', newF
+        print 'new file', newF, oldF
 
-def copyFiles(orgF,newF):
+def copyFiles(oldF,newF):
     if os.path.exists(newF):
-        if not filecmp.cmp(orgF,newF):
+        if not filecmp.cmp(oldF,newF):
             print 'update', newF
-            shutil.copy(orgF,newF)
+            shutil.copy(oldF,newF)
     else:
         print 'new file', newF
-        shutil.copy(orgF,newF)
+        shutil.copy(oldF,newF)
         os.system("svn add "+newF)
 
 def updateFolder(fld1,dryRun=True):
@@ -39,6 +40,8 @@ def updateFolder(fld1,dryRun=True):
         if newroot == '':
             for f in files:
                 if f in ['configure','Makefile','Doxyfile']:
+                    continue
+                elif '.csh' in f or '.lis' in f:
                     continue
                 elif f in ['COPYING','README']:
                     newroot=''
@@ -86,14 +89,19 @@ def updateFolder(fld1,dryRun=True):
                 continue
             if '.tar' in f:
                 continue
-            if os.access(root+"/"+f, os.X_OK) and ('source' in root or 'validation' in root):
+            if '.csh' in f:
+                continue
+            if os.access(root+"/"+f, os.X_OK) and ('source' in root or 'validation' in root) and 'cpp' not in f:
                 continue
             if dryRun:
                 compareFiles(root+"/"+f,newroot+'/'+f)
             else:
                 copyFiles(root+"/"+f,newroot+'/'+f)
     if not dryRun:
+        updateMakefile(currentPhosim)
         updateVersion(fld1)
+    else:
+        compareMakefile(currentPhosim)
 
 def cpFolder(fld1):
     for root, dirs, files in os.walk(fld1):
@@ -136,31 +144,64 @@ def addVersion(fld1):
     os.chdir(currentDir)
     os.system("svn add bin/version")
 
+def compareMakefile(fld1):
+    dirs = [f for f in os.listdir(fld1+'source/') if '.tar' not in f and '.git' not in f]
+    for dir in dirs:
+        oldMF=fld1+'source/'+dir+'/makefile'
+        if not os.path.exists(oldMF):
+            oldMF=fld1+'source/'+dir+'/Makefile'
+        newMF='src/source/'+dir+'/makefile'
+        if not os.path.exists(newMF):
+            newMF='src/source/'+dir+'/Makefile'
+        oldF=open(oldMF).readlines()
+        newF=open(newMF).readlines()
+        result=difflib.unified_diff(oldF,newF)
+        resultStr=''.join(list(result))
+        print resultStr
 
-def addVersion(fld1):
-    currentDir=os.path.split(os.path.abspath(__file__))[0]
-    os.chdir(fld1)
-    os.system("git log -1 --format=commit' '%H > "+currentDir+"/bin/version")
-    os.system("git describe --tags >> "+currentDir+"/bin/version")
-    os.chdir(currentDir)
-    os.system("svn add bin/version")
+def updateMakefile(fld1):
+    dirs = [f for f in os.listdir(fld1+'source/') if '.tar' not in f and '.git' not in f]
+    for dir in dirs:
+        oldMF=fld1+'source/'+dir+'/makefile'
+        if not os.path.exists(oldMF):
+            oldMF=fld1+'source/'+dir+'/Makefile'
+        newMF='src/source/'+dir+'/makefile'
+        if not os.path.exists(newMF):
+            newMF='src/source/'+dir+'/Makefile'
+        f=open(newMF,"r+")
+        newd=f.readlines()
+        f.seek(0)
+        i=0
+        for line in open(oldMF).readlines():
+            if 'cp ' in line:
+                continue
+            if 'CFLAGS' in line and 'CC' not in line:
+                while 'CFLAGS' not in newd[i]:
+                    i+=1
+                f.write(newd[i])
+                continue
+            if 'LFLAGS' in line and 'CC' not in line:
+                while 'LFLAGS' not in newd[i]:
+                    i+=1
+                f.write(newd[i])
+                continue
+            f.write(line)
+        f.truncate()
+        f.close()
 
 
-#listFolder('phosim/source')
-#listFolderW('../../phosim/source')
-#cpFolder('../../phosim/')
-if len(sys.argv)<2:
-    updateFolder('/home/epeng/phosim/')
+currentPhosim=sys.argv[1]
+if not os.path.exists(currentPhosim):
+    print currentPhosim, 'does not exist'
+    sys.exit()
+if currentPhosim[-1]!='/':
+    currentPhosim+='/'
+
+if len(sys.argv)<3:
+    updateFolder(currentPhosim)
 else:
-    if sys.argv[1] == 'False':
-        updateFolder('/home/epeng/phosim/',dryRun=False)
+    if sys.argv[2] == 'False':
+        updateFolder(currentPhosim,dryRun=False)
     else:
-        updateFolder('/home/epeng/phosim/')
+        updateFolder(currentPhosim)
 
-#addVersion('../../phosim/')
-
-
-#change source to src
-# 1. configure
-# 2. mv Makefile to src/.
-# 3. update src/Makefile, validation/Makefile, validation/unittest.cpp
